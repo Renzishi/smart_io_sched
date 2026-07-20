013c03fd154e643e205189af863511ab3845b9e6
renzis <hhsdsryx@qq.com>
2026-07-20 14:40:51 +0800

    init scheduler

---
+++ smart_deadline.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef SMART_DEADLINE_H
+#define SMART_DEADLINE_H
+
+#include <linux/types.h>
+
+int smart_deadline_init(void);
+void smart_deadline_exit(void);
+
+#endif
